02cdf2226bb7841cde5d9bdf960f89816ac7ff11
ShereinDabbah <47598724+ShereinDabbah@users.noreply.github.com>
2026-09-22 11:02:51 +0300

    Committing 1 item from workspace 17cd6fb8-4014-4a56-82b7-70532af2b7ce

---
+++ GitFolderTest2/Notebook_CICDTest.Notebook/notebook-content.py
@@ -0,0 +1,35 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "63e71bea-7fd1-4c8f-94ae-5dfd00511c35",
+# META       "default_lakehouse_name": "Lakehouse1",
+# META       "default_lakehouse_workspace_id": "17cd6fb8-4014-4a56-82b7-70532af2b7ce",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "63e71bea-7fd1-4c8f-94ae-5dfd00511c35"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
